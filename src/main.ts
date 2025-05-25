ca063d76c3e8bb7db913cb09c81bb8bb4434b90a
Adisorn Kaewsermwong <adisorn.ksw@gmail.com>
2025-05-25 21:43:47 +0700

    add main import
    - import swagger
    - import validation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,30 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
+  // import validation lib
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  
   await app.listen(process.env.PORT ?? 3000);
+
+  // import swagger lib
+  const config = new DocumentBuilder()
+    .setTitle('My API')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .addTag('example')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
 }
 bootstrap();
